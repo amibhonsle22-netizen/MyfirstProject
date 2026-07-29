0ebd1762b1ce82247163c73b17ffe1457c5fbe4c
amibhonsle22-netizen <amibhonsle22@gmail.com>
2026-07-29 21:42:05 +0530

    initial commit

---
+++ Loops.R
@@ -0,0 +1,36 @@
+library(usethis)
+use_git()
+i=1
+while(i<10){
+  #print(i)
+  i=i+1
+  print(i)
+}
+
+i = 1
+while(i<10){
+  #print(i)
+  i=i+1
+  print(i)
+}
+
+i = 1
+while(i<=10){
+  #print(i)
+  i=i+1
+  print(i)
+}
+
+i = 0
+while(i<10){
+  print(paste(i,"before incrementing"))
+  i=i+1
+  print(paste(i,"after incrementing"))
+}
+
+i = 0
+while(i<10){
+  print(paste(i,"th iteration: before incrementing"))
+  i=i+1
+  print(paste(i,"th iteration: after incrementing"))
+}
