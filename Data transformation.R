6dffdc73c08031f74346faa8f671be2d31a50504
amibhonsle22-netizen <amibhonsle22@gmail.com>
2026-07-29 20:54:06 +0530

    Test commit

--- Data transformation.R
+++ Data transformation.R
@@ -18,3 +18,41 @@ flights |>
 
 flights |>
   filter(month == 1 | month == 2)
+
+flights |>
+  filter(month == 2 | day == 1)
+
+flights |>
+  filter(month == 2 & day == 1)
+
+# A shorter way to select flights that departed in January or February
+flights |>
+  filter(month %in% c(1, 2)) ->C
+
+flights |>
+  filter(month %in% 1) ->A
+flights |>
+  filter(month %in% 2) ->B
+
+dim(C)
+dim(B)
+dim(A)
+
+flights |>
+  arrange(year, month, day, dep_time)
+
+flights |>
+  arrange(desc(dep_delay))
+
+#distinct()
+# Remove duplicate rows, if any
+flights |>
+  distinct(origin, dest)
+
+#mutate
+flights |>
+  mutate(
+    gain = dep_delay - arr_delay,
+    speed = distance / air_time * 60,
+    .before = 1
+  )
